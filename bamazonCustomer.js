f87630aea0cb480df9b3f13083f231c5eca34822
Elaine Cadman <elaine.cadman@gmail.com>
2018-12-08 12:28:38 -0800

    basics done

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,8 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
 var divider = "***---------------------------------***";
+var orderTotal = 0;
+var newStock = 0;
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -33,56 +35,49 @@ connection.connect(function(err) {
       });
   }
 
-  function chooseItem(){
-      inquirer.prompt([
-          {
-              name: "choice",
+function chooseItem() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        inquirer.prompt([
+            {
+                name: "choice",
+                type: "input",
+                message: "Please choose an item to buy by entering its item_id."
+            },
+            {
+              name: "numberToBuy",
               type: "input",
-              message: "Please choose an item to buy by entering its item_id."
-          },
-          {
-            name: "numberToBuy",
-            type: "input",
-            message: "How many would you like?"
-        }
-      ])
-      .then(function(answer){
-          
-          connection.query("SELECT * FROM products WHERE ?", {item_id: answer.choice}, function(err, res){
-            console.log(divider);
-            console.log(
-                "Item: " + res[0].product_name+ "\nPrice: $"+ res[0].price 
-            );
-            console.log(divider);
-
-            if (answer.numberToBuy > res[0].stock_quantity){
+              message: "How many would you like?"
+          }
+        ])
+        .then(function(answer){
+            orderTotal = answer.numberToBuy*res[0].price;
+            newStock = res[0].stock_quantity-answer.numberToBuy;
+            var chosenItem;
+            chosenItem = answer.choice;
+            
+            if(answer.numberToBuy > res[0].stock_quantity){
                 console.log("Oh, sorry, Santa just came by so we don't have enough of that item to fill your order!");
             }else{
-                console.log("Ok, let me get the elves working on your order!");
-                
-                let newStock = res[0].stock_quantity-answer.numberToBuy;
-                let orderTotal = answer.numberToBuy*res[0].price;
-
                 console.log("Thanks for shopping at Bamazon! Your order total is: $"+ orderTotal);
-
-                // connection.query("UPDATE products SET ? WHERE ?",
-                // [
-                //     {
-                //         stock_quantity: newStock
-                //     }
-                // ],
-                // function(err, res){
-                //     if (err) throw err;
-                //    console.log(res);
-                // }
-                // );
+                connection.query(
+                    "UPDATE products SET ? WHERE ?",
+                    [
+                        {
+                            stock_quantity: newStock
+                        },
+                        {
+                            item_id: chosenItem
+                        }
+                    ], 
+                    function(err){
+                        if (err) throw err;
+                    }
+                )
+                connection.end();
             }
-            
-        //   console.log(answer);
-
-        //   console.log("#" + answer.numberToBuy);
-          
-          connection.end();
-      });
-  });
+        })
+        
+    })
 }
+
