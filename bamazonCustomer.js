8dbba25a53e222523b1dc4064c0976c4687b983b
Elaine Cadman <elaine.cadman@gmail.com>
2018-12-10 09:47:28 -0800

    customer fixed

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -49,17 +49,18 @@ function chooseItem() {
               type: "input",
               message: "How many would you like?"
           }
-        ])
-        .then(function(answer){
-            orderTotal = answer.numberToBuy*res[0].price;
-            newStock = res[0].stock_quantity-answer.numberToBuy;
-            var chosenItem;
-            chosenItem = answer.choice;
+        ])        .then(function(answer){
+            // var chosenItem;
+            var chosenItem = answer.choice-1;
+            orderTotal = answer.numberToBuy*res[chosenItem].price;
+            newStock = res[chosenItem].stock_quantity-answer.numberToBuy;
+            // console.log(chosenItem, orderTotal, newStock);
+            
             
             if(answer.numberToBuy > res[0].stock_quantity){
                 console.log("Oh, sorry, Santa just came by so we don't have enough of that item to fill your order!");
             }else{
-                console.log("Thanks for shopping at Bamazon! Your order total is: $"+ orderTotal);
+                console.log("Thanks for your purchase of " + answer.numberToBuy + " " + res[chosenItem].product_name + " at Bamazon! Your order total is: $"+ orderTotal);
                 connection.query(
                     "UPDATE products SET ? WHERE ?",
                     [
@@ -67,17 +68,66 @@ function chooseItem() {
                             stock_quantity: newStock
                         },
                         {
-                            item_id: chosenItem
+                            item_id: answer.choice
                         }
+                        
                     ], 
                     function(err){
                         if (err) throw err;
+                        connection.end(); 
                     }
                 )
-                connection.end();
+                
             }
         })
-        
+        // connection.end(); 
     })
+    
 }
+//         .then(function(answer){
+//             console.log(answer);
+//             ;
+//             // console.log(res[chosenItem]);
+//             var chosenItem = parseFloat(answer.choice-1);
+//             var qty = parseFloat(answer.numberToBuy-1);
+//             var orderTotal = res[chosenItem].price*answer.numberToBuy;
+//             var prevSales = res[chosenItem].product_sales;
+//             var newSales = prevSales+orderTotal;
+//             var newStock = res[chosenItem].stock_quantity-answer.numberToBuy;
+//             console.log("choices", orderTotal, prevSales, newSales, newStock);
+
+//             // prevSales = res[chosenItem].product_sales;
+//             // orderTotal = res[chosenItem].numberToBuy*res[chosenItem].price;
+//             // newStock = res[chosenItem].stock_quantity-res[chosenItem].numberToBuy;
+//             // newSales = prevSales + orderTotal;
+//             // console.log("new sales: ", newSales);       
+            
+    
+//             if(answer.numberToBuy > answer.choice.stock_quantity){
+//                 console.log("Oh, sorry, Santa just came by so we don't have enough of that item to fill your order!");
+//             }else{
+//                 console.log("Thanks for shopping at Bamazon! Your order total is: $"+ orderTotal);
+//                 connection.query(
+//                     "UPDATE products SET ?,? WHERE ?",
+//                     [
+//                         {
+//                             stock_quantity: newStock
+//                         },
+//                         {
+//                             product_sales: orderTotal
+//                         },
+//                         {
+//                             item_id: chosenItem
+//                         }
+//                     ], 
+//                     function(err){
+//                         if (err) throw err;
+//                     }
+//                 )
+//                 connection.end();
+//             }
+//         })
+        
+//     })
+// }
 
