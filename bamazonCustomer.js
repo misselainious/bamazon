63e7adeefdb87044f94c71bbceb1fc8f99560d82
Elaine Cadman <elaine.cadman@gmail.com>
2018-12-11 16:41:50 -0800

    supervisor working no profit

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,72 +1,73 @@
+//Requirements
 var mysql = require('mysql');
 var inquirer = require('inquirer');
+//Variables
 var divider = "***---------------------------------***";
 var orderTotal = 0;
 var newStock = 0;
 
+//connecting to database
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
-  
+
     // Your username
     user: "root",
-  
+
     // Your password
     password: "flute0207",
     database: "bamazon"
-  });
-  
+});
 
-connection.connect(function(err) {
+
+connection.connect(function (err) {
     if (err) throw err;
     // console.log("connected as id " + connection.threadId);
     console.log(divider);
     console.log("Welcome to Bamazon! Your one stop shop for all your Holiday Shopping. \nPlease browse our available products:")
     displayItems();
-  });
+});
 
-  function displayItems(){
-      connection.query("SELECT item_id,product_name,price FROM products", function(err, res){
-          if (err) throw err;
-          let data = res;
-            console.table(data);
-            console.log(divider);
-          chooseItem();
-      });
-  }
+function displayItems() {
+    connection.query("SELECT item_id,product_name,price FROM products", function (err, res) {
+        if (err) throw err;
+        let data = res;
+        console.table(data);
+        console.log(divider);
+        chooseItem();
+    });
+}
 
 function chooseItem() {
-    connection.query("SELECT * FROM products", function(err, res) {
+    connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
-        inquirer.prompt([
-            {
+        inquirer.prompt([{
                 name: "choice",
                 type: "input",
                 message: "Please choose an item to buy by entering its item_id."
             },
             {
-              name: "numberToBuy",
-              type: "input",
-              message: "How many would you like?"
-          }
-        ])        .then(function(answer){
+                name: "numberToBuy",
+                type: "input",
+                message: "How many would you like?"
+            }
+        ]).then(function (answer) {
             // var chosenItem;
-            var chosenItem = answer.choice-1;
-            orderTotal = answer.numberToBuy*res[chosenItem].price;
-            newStock = res[chosenItem].stock_quantity-answer.numberToBuy;
+            var chosenItem = answer.choice - 1;
+            orderTotal = answer.numberToBuy * res[chosenItem].price;
+            newStock = res[chosenItem].stock_quantity - answer.numberToBuy;
             prevSales = res[chosenItem].product_sales;
             var newSales = prevSales + orderTotal;
             // console.log(chosenItem, orderTotal, newStock);
-            
-            
-            if(answer.numberToBuy > res[0].stock_quantity){
+
+
+            if (answer.numberToBuy > res[0].stock_quantity) {
                 console.log("Oh, sorry, Santa just came by so we don't have enough of that item to fill your order!");
-            }else{
-                console.log("Thanks for your purchase of " + answer.numberToBuy + " " + res[chosenItem].product_name + " at Bamazon! Your order total is: $"+ orderTotal);
+            } else {
+                console.log("Thanks for your purchase of " + answer.numberToBuy + " " + res[chosenItem].product_name + " at Bamazon! Your order total is: $" + orderTotal);
                 connection.query(
                     "UPDATE products SET ?,? WHERE ?",
-                    [
-                        {
+                    [{
                             stock_quantity: newStock
                         },
                         {
@@ -75,19 +76,19 @@ function chooseItem() {
                         {
                             item_id: answer.choice
                         }
-                        
-                    ], 
-                    function(err){
+
+                    ],
+                    function (err) {
                         if (err) throw err;
-                        connection.end(); 
+                        connection.end();
                     }
                 )
-                
+
             }
         })
-        // connection.end(); 
+        
     })
-    
+
 }
 //         .then(function(answer){
 //             console.log(answer);
@@ -106,8 +107,8 @@ function chooseItem() {
 //             // newStock = res[chosenItem].stock_quantity-res[chosenItem].numberToBuy;
 //             // newSales = prevSales + orderTotal;
 //             // console.log("new sales: ", newSales);       
-            
-    
+
+
 //             if(answer.numberToBuy > answer.choice.stock_quantity){
 //                 console.log("Oh, sorry, Santa just came by so we don't have enough of that item to fill your order!");
 //             }else{
@@ -132,7 +133,6 @@ function chooseItem() {
 //                 connection.end();
 //             }
 //         })
-        
+
 //     })
 // }
-
---
+++ supervisor.js
@@ -0,0 +1,34 @@
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+var divider = "***---------------------------------***";
+
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "flute0207",
+    database: "bamazon"
+  });
+  
+
+connection.connect(function(err) {
+    if (err) throw err;
+    // console.log("connected as id " + connection.threadId);
+    console.log(divider);
+    console.log("Hey there, you Super Supervisor! \nLet me pull up some stats for you...");
+    profit();
+  });
+
+  function profit(){
+    connection.query("SELECT departments.department_id, products.department_name, products.product_sales, departments.over_head_costs FROM products INNER JOIN departments ON products.department_name=departments.department_name GROUP BY department_name ORDER BY SUM(product_sales) DESC", function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        console.log(divider);
+        connection.end();
+    });
+  }
