49dde47647f59ff30fd0f7fc22b58866771dcda4
Elaine Cadman <elaine.cadman@gmail.com>
2018-12-12 16:48:36 -0800

    supervisor update

--- supervisor.js
+++ supervisor.js
@@ -18,11 +18,36 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    // console.log("connected as id " + connection.threadId);
     console.log(divider);
-    console.log("Hey there, you Super Supervisor! \nLet me pull up some stats for you...");
-    profit();
+    inquire();
   });
+
+  function inquire(){
+    inquirer.prompt([{
+        type: 'list',
+        message: 'Hey there, you Super Supervisor! \nWhat do you need to do today?',
+        name: 'command',
+        choices: ['View product sales by department', 'Create a new department'],
+        validate: function(answer){
+            if(answer.length<1){
+                return 'You must choose an option, you are at work after all...'
+            }
+            return true;
+            }
+        }
+    ])
+    .then(answer=>{
+        switch(answer.command){
+            case 'View product sales by department':
+            profit();
+            break;
+  
+            case 'Create a new department':
+            createDepartment();
+            break;
+        }
+    })
+    }
 
   function profit(){
     connection.query("SELECT departments.department_id, products.department_name, products.product_sales, departments.over_head_costs, (products.product_sales - departments.over_head_costs) AS total_profit FROM products INNER JOIN departments ON products.department_name=departments.department_name GROUP BY department_name ORDER BY SUM(product_sales) DESC", function (err, res) {
@@ -32,4 +57,9 @@ connection.connect(function(err) {
         console.log(divider);
         connection.end();
     });
+  }
+
+  function createDepartment(){
+    console.log("Yeah...working on that...");
+    connection.end();
   }
